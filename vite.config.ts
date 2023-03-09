9136195df977c81b2ad9274f51ceb86660cb5d88
azertypow <azertypow@gmail.com>
2023-03-09 15:27:58 +0100

    feat: PWA minimal setup

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,29 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
       vue(),
-      VitePWA({registerType: 'autoUpdate', mode: 'production'})
+      VitePWA({
+          registerType: 'autoUpdate',
+          mode: 'production',
+          includeAssets: ['favicon.ico', 'logo.png', 'logo.svg'],
+          manifest: {
+              name: 'Calculateur Carbone Campus',
+              short_name: 'CalculateurCarboneCampus',
+              description: 'Calculateur Carbone Campus, HEAD et HEG project',
+              theme_color: '#ffffff',
+              icons: [
+                  {
+                      src: 'logo.png',
+                      sizes: '192x192',
+                      type: 'image/png'
+                  },
+                  {
+                      src: 'logo.png',
+                      sizes: '512x512',
+                      type: 'image/png'
+                  }
+              ]
+          }
+      })
   ],
   resolve: {
     alias: {
